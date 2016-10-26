f83a263db7a9e58248334d0ad6203a9b6fbee6b3
Alex Gessner <alex.gessner@gmail.com>
2016-10-26 14:46:56 +0100

    update and lock down requirements

--- setup.py
+++ setup.py
@@ -1,20 +1,31 @@
 from setuptools import setup, find_packages
 
 
+running_requirements =[
+    'requests==2.7.0',
+    'httpretty==0.8.10',
+    'boto==2.34.0',
+    'botocore==1.3.29',
+    'psycopg2==2.6',
+    'SQLAlchemy==1.0.5',
+    'Cython==0.24.1',
+    'pandas>=0.14.0'
+]
+
+test_requirements = [
+    'freezegun==0.3.5',
+    'moto==0.4.1',
+    'nose==1.3.7',
+    'rednose==0.4.3'
+]
+
 setup(
     name='aws_etl_tools',
     description='some helpers for getting your data into redshift',
     url='https://github.com/shopkeep/aws_etl_tools',
     author_email='data@shopkeep.com',
-    install_requires=[
-        'requests==2.7.0',
-        'boto==2.34.0',
-        'botocore==1.3.29',
-        'psycopg2==2.6',
-        'SQLAlchemy==1.0.5',
-        'Cython==0.24.1',
-        'pandas>=0.14.0'
-    ],
+    install_requires=running_requirements,
+    tests_require=running_requirements + test_only_requirements,
     version='0.0.1',
     packages=find_packages(),
     test_suite='nose.collector'
