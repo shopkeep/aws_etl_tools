28a98425110a3e20954c7cd5e7476921b0b04cfd
Alex Gessner <alex.gessner@gmail.com>
2016-12-21 14:56:22 -0500

    add and structure new exceptions

--- aws_etl_tools/exceptions.py
+++ aws_etl_tools/exceptions.py
@@ -1,3 +1,13 @@
-class NoDataFoundError(Exception):
+class BaseAwsEtlToolsError(RuntimeError):
+    def __init__(self, message):
+        super().__init__(message)
+
+
+class NoDataFoundError(BaseAwsEtlToolsError):
+    def __init__(self, message):
+        super().__init__(message)
+
+
+class NoS3BasePathError(BaseAwsEtlToolsError):
     def __init__(self, message):
         super().__init__(message)
